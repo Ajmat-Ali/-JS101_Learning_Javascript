accef047d585d4336b656f388d42629b92def7f2
Ajmat-Ali <badshah9631619358@gmail.com>
2023-10-27 14:39:12 +0530

    Variable Assignment Added

---
+++ variable.js
@@ -0,0 +1,26 @@
+console.log("   ")
+console.log("|-------------------- 𝕣𝕖𝕡𝕠𝕣𝕥 𝕔𝕒𝕣𝕕 --------------------") 
+let name="Ajmat Ali";
+let schoolName="Masai School";
+let grade="A";
+let sec="B5";
+let roll=23;
+let html=67;
+let css=76;
+let js=89;
+console.log("|   ")
+console.log("|      Name    -    "+ name)
+console.log("|      Roll No -    "+roll)
+console.log("|      Section -    "+sec)
+console.log("|      School  -    "+schoolName)
+console.log("|      Grade   -    "+grade)
+console.log("|   ")
+console.log("|---------------------- 𝗠𝗮𝗿𝗸𝘀 ----------------------")
+console.log("|   ")
+console.log("|   HTML        -     "+html)
+console.log("|   CSS         -     "+css)
+console.log("|   JAVASCRIPT  -     "+js)
+console.log("|")
+console.log("|")
+console.log("")
+console.log("")
